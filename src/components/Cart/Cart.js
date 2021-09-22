79236dd47eba6fbcf81f6f24ac4b52d54e4b7a9b
Tahmid Islam <tahmid231@gmail.com>
2021-09-22 23:11:29 +0600

    Tax and shipping fee added in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,11 +9,18 @@ const Cart = (props) => {
   //   }
   const total = cart.reduce((previous, product) => previous + product.price, 0);
 
+  const shipping = 15;
+  const tax = (total + shipping) * 10;
+  const grandTotal = total + shipping + tax;
+
   return (
     <div>
       <h3>Order Summary</h3>
       <h5>Items Ordered: {cart.length}</h5>
-      <p>Total: {total}</p>
+      <p>Total: {total.toFixed(2)}</p>
+      <p>Shipping: {shipping} </p>
+      <p>Tax: {tax.toFixed(2)} </p>
+      <p>Grand Total: {grandTotal.toFixed(2)}</p>
     </div>
   );
 };
