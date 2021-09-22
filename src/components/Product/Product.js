c229427b9044c955cdc7a28ac8e5c0e25ba598db
Tahmid Islam <tahmid231@gmail.com>
2021-09-22 22:54:22 +0600

    cart total price and fontawesome added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const Product = (props) => {
+  const { name, img, seller, price, stock } = props.product;
+  const { handleAddToCart } = props;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <h4 className="product-name">{name}</h4>
+        <p>
+          <small>by: {seller}</small>
+        </p>
+        <p>Price: {price}</p>
+        <p>
+          <small>Only {stock} left in stock - order soon</small>
+        </p>
+        <button
+          className="btn-regular"
+          onClick={() => handleAddToCart(props.product)}
+        >
+          <FontAwesomeIcon icon={faShoppingCart} />
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
